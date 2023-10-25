27782fae8670d46edaba6580f0d16f2f155ee757
qegloff <quentin.egloff@gmail.com>
2023-10-25 16:13:04 +0200

    exporting as picture is working

--- postprocessor.py
+++ postprocessor.py
@@ -23,13 +23,13 @@ class PostProcessor:
         matrix = self.solver.u[1:-1, 1:-1].round(dec)
         np.savetxt(self.path + "/" + name + "_t" + str(round(self.solver.crt_time,2)) + ext, matrix, delimiter=",")
 
-    def export_img(self, name = "heat_export", ext=".png", dec = 2):
+
+    def export_img(self, name="heat_export", ext=".png", dec=2):
         matrix = self.solver.u[1:-1, 1:-1].round(dec).copy()
-        fig = plt.subplot
-        img = fig.imshow(matrix, cmap=plt.get_cmap('bwr'), vmin=matrix.min(),vmax=matrix.max())
-        img.axis('off')
-        img.title('Result at t={:.1f} s'.format(round(self.solver.crt_time,2)))
-        #cbar_plt = plt.add_axes([0.9, 0.15, 0.03, 0.7])
-        #cbar_plt.set_xlabel('$T$ / K', labelpad=20)
-        #plt.colorbar(im, cplt=cbar_plt)
+        fig = plt.figure()
+        img = plt.imshow(matrix, cmap=plt.get_cmap('bwr'), vmin=matrix.min(), vmax=matrix.max())
+        plt.axis('off')
+        plt.title('Result at t={:.1f} s'.format(round(self.solver.crt_time, 2)))
+        color_bar = plt.colorbar(img)
+        color_bar.set_label('Thermal Scale')
         plt.show()
